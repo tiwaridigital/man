a94effa812eb0b0968ad5336562fc8f27ac8e7bc
tiwaridigital <111147384+tiwaridigital@users.noreply.github.com>
2023-11-11 00:16:31 +0530

    data fetch for admin complete

--- src/app/components/admin/Admin.jsx
+++ src/app/components/admin/Admin.jsx
@@ -1,12 +1,15 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ContentWrapper from '../contentWrapper/ContentWrapper'
 import Link from 'next/link'
 import Select from 'react-select'
 import Modal from '../modal/Modal'
+import { myAction } from '@/app/actions/actions'
 
-const Admin = ({ data }) => {
-  console.log('data', data)
+const Admin = () => {
+  const [manga, setManga] = useState(null)
+  console.log('manga', manga)
+
   const [open, setOpen] = useState(false)
   const [srcUrl, setSrcUrl] = useState(null)
   const options = [
@@ -28,14 +31,15 @@ const Admin = ({ data }) => {
     }
   }
 
-  const handleSources = (e) => {
-    if (e.value === 'mangadex') {
-      console.log('mangadex')
-      /*
-       * First Fetch The Manga Using getManga Function
-       */
-
-    }
+  const handleSources = async (e) => {
+    console.log('handleSources')
+    /*
+     * First Fetch The Manga Using myAction Function, which is a => Server Action
+     * i.e. fucntion based on 'use server' method -> where you can call other server functions
+     * and then use those not directly supported server functions/methods -> indirectly
+     */
+    const data = await myAction(e.value)
+    setManga(data)
   }
 
   return (
@@ -48,7 +52,13 @@ const Admin = ({ data }) => {
             onChange={handleActions}
           />
         </div>
-        <Modal setOpen={setOpen} open={open} options={mangaSources} onChange={handleSources} setSrcUrl={setSrcUrl}/>
+        <Modal
+          setOpen={setOpen}
+          open={open}
+          options={mangaSources}
+          onChange={handleSources}
+          setSrcUrl={setSrcUrl}
+        />
       </ContentWrapper>
     </div>
   )
