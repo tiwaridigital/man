9c7a18c9d9ee697446fd2882a5ef618359b3f256
Ujjwal <ujwltiwari494@gmail.com>
2023-11-14 23:09:26 +0530

    all manga fetched on home page, manga details fetched using server

--- src/app/details/[chapter]/page.js
+++ src/app/details/[chapter]/page.js
@@ -1,34 +1,34 @@
 'use client'
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import client from '../../../../client'
-import SINGLE_CHAPTER_QUERY from '@/graphql/adm/chapters/SingleChapterQuery.gql'
-import SingleChapter from '@/components/details/singleChapter/SingleChapter';
+import SINGLE_CHAPTER_QUERY from '@/graphql/admin/chapters/SingleChapterQuery.gql'
+import SingleChapter from '@/components/details/singleChapter/SingleChapter'
 
 const Page = () => {
-    const [chapterData, setChapterData] = useState(null)
+  const [chapterData, setChapterData] = useState(null)
 
-    const fetchChapter = async () => {
-        const result = await client.query({
-            query: SINGLE_CHAPTER_QUERY,
-            variables: {
-                // url: 'b6198714-7c53-4ff7-bd5d-58f9900eaa22',
-                slug: 'i-killed-an-academy-player-chapter-6'
-            },
-        })
-        console.log('chapter', result)
-        setChapterData(result.data.chapters)
-    }
-    // console.log('chapterData', chapterData)
+  const fetchChapter = async () => {
+    const result = await client.query({
+      query: SINGLE_CHAPTER_QUERY,
+      variables: {
+        // url: 'b6198714-7c53-4ff7-bd5d-58f9900eaa22',
+        slug: 'i-killed-an-academy-player-chapter-6',
+      },
+    })
+    console.log('chapter', result)
+    setChapterData(result.data.chapters)
+  }
+  // console.log('chapterData', chapterData)
 
-    useEffect(() => {
-        fetchChapter()
-    }, [])
+  useEffect(() => {
+    fetchChapter()
+  }, [])
 
-    return (
-        <div>
-            <SingleChapter chapter={chapterData}/>
-        </div>
-    )
+  return (
+    <div>
+      <SingleChapter chapter={chapterData} />
+    </div>
+  )
 }
 
 export default Page
---
+++ src/app/manga/[slug]/loading.js
@@ -0,0 +1,8 @@
+import BannerSkelton from '@/components/details/detailsBannerDex/BannerSkelton'
+import React from 'react'
+
+const Loading = () => {
+  return <BannerSkelton />
+}
+
+export default Loading
