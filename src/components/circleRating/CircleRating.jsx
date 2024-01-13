892d3452ec45690da397b33c6c3b7a009026c3d0
tiwaridigital <111147384+tiwaridigital@users.noreply.github.com>
2024-01-13 08:04:02 +0530

    rating stroke changed to graident

--- src/components/circleRating/CircleRating.jsx
+++ src/components/circleRating/CircleRating.jsx
@@ -1,22 +1,84 @@
-import React from 'react'
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
-import 'react-circular-progressbar/dist/styles.css'
+import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-import './style.scss'
+import './style.scss';
 
 const CircleRating = ({ rating }) => {
   return (
-    <div className='circleRating'>
+    <div className="circleRating">
+      {rating < 5 ? (
+        <YellowGradientSVG />
+      ) : rating < 7 ? (
+        <OrangeGradientSVG />
+      ) : (
+        <GreenGradientSVG />
+      )}
       <CircularProgressbar
         value={rating}
         maxValue={10}
         text={rating}
-        styles={buildStyles({
-          pathColor: rating < 5 ? 'red' : rating < 7 ? 'orange' : 'green',
-        })}
+        // styles={buildStyles({
+        //   pathColor: rating < 5 ? 'red' : rating < 7 ? 'orange' : 'green',
+        // })}
+        styles={{
+          path: {
+            stroke: `url(#${
+              rating < 5 ? 'red' : rating < 7 ? 'orange' : 'green'
+            })`,
+            height: '100%',
+          },
+          trail: {
+            stroke: '#2e2e2e',
+          },
+        }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default CircleRating
+export default CircleRating;
+
+function OrangeGradientSVG() {
+  const idCSS = 'orange';
+  const gradientTransform = 'rotate(90)';
+  return (
+    <svg style={{ height: 0 }}>
+      <defs>
+        <linearGradient id={idCSS} gradientTransform={gradientTransform}>
+          <stop offset="0.99%" stopColor="#f89e00" />
+          <stop offset="100%" stopColor="#da2f68" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+function GreenGradientSVG() {
+  const idCSS = 'green';
+  const gradientTransform = 'rotate(90)';
+  return (
+    <svg style={{ height: 0 }}>
+      <defs>
+        <linearGradient id={idCSS} gradientTransform={gradientTransform}>
+          <stop offset="0.99%" stopColor="#8cc34e" />
+          <stop offset="100%" stopColor="#00a96f" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+function YellowGradientSVG() {
+  const idCSS = 'red';
+  const gradientTransform = 'rotate(90)';
+  return (
+    <svg style={{ height: 0 }}>
+      <defs>
+        <linearGradient id={idCSS} gradientTransform={gradientTransform}>
+          <stop offset="0%" stopColor="rgb(254, 240, 138)" />
+          <stop offset="50%" stopColor="rgb(250, 204, 21)" />
+          <stop offset="100%" stopColor="rgb(171, 138, 17)" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
--- src/components/explore/Explore.jsx
+++ src/components/explore/Explore.jsx
@@ -1,24 +1,24 @@
-'use client'
-import React, { useState, useEffect } from 'react'
+'use client';
+import React, { useState, useEffect } from 'react';
 // import { useParams } from 'react-router-dom'
 // import InfiniteScroll from 'react-infinite-scroll-component'
-import Select from 'react-select'
+import Select from 'react-select';
 
-import './style.scss'
+import './style.scss';
 
 // import useFetch from '../../hooks/useFetch'
 // import { fetchMoviesFromApi } from '../../utils/api'
-import MovieCard from '../../components/movieCard/MovieCard'
-import Spinner from '../../components/spinner/Spinner'
-import ContentWrapper from '../contentWrapper/ContentWrapper'
-import Genres from '../filters/Genres/Genres'
-import Status from '../filters/Status/Status'
-import DownArrow from '../../../public/assets/icons/DownArrow'
-import Added from '../filters/Added/Added'
-import Pagination from '../pagination/Pagination'
+import MovieCard from '../../components/movieCard/MovieCard';
+import Spinner from '../../components/spinner/Spinner';
+import ContentWrapper from '../contentWrapper/ContentWrapper';
+import Genres from '../filters/Genres/Genres';
+import Status from '../filters/Status/Status';
+import DownArrow from '../../../public/assets/icons/DownArrow';
+import Added from '../filters/Added/Added';
+import Pagination from '../pagination/Pagination';
 // import Pagination from '../pagination/Pagination'
 
-let filters = {}
+let filters = {};
 
 const sortbyData = [
   { value: 'popularity.desc', label: 'Popularity Descending' },
@@ -31,44 +31,39 @@ const sortbyData = [
   },
   { value: 'primary_release_date.asc', label: 'Release Date Ascending' },
   { value: 'original_title.asc', label: 'Title (A-Z)' },
-]
+];
 
 const Explore = ({ manga }) => {
-  const [data, setData] = useState([])
-  const [pageNum, setPageNum] = useState(1)
-  const [loading, setLoading] = useState(false)
-  const [genre, setGenre] = useState(null)
-  const [sortby, setSortby] = useState(null)
-  const { mediaType } = 'movies'
-  const [selectedGenres, setSelectedGenres] = useState([])
-  const [showGenres, setShowGenres] = useState(false)
-  const [selectedStatus, setSelectedStatus] = useState(null)
-  const [showStatus, setShowStatus] = useState(false)
-  const [selectedAdded, setSelectedAdded] = useState(null)
-  const [showAdded, setShowAdded] = useState(false)
+  const [data, setData] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [genre, setGenre] = useState(null);
+  const [sortby, setSortby] = useState(null);
+  const { mediaType } = 'movies';
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [showGenres, setShowGenres] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState(null);
+  const [showStatus, setShowStatus] = useState(false);
+  const [selectedAdded, setSelectedAdded] = useState(null);
+  const [showAdded, setShowAdded] = useState(false);
   const [showFilters, setShowFilters] = useState({
     genres: false,
     status: false,
     added: false,
-  })
+  });
   /*
    * Pagination
    */
-  const itemsPerPage = 10
-  const [currentPage, setCurrentPage] = useState(1)
-  const totalPages = Math.ceil(data?.length / itemsPerPage)
-  const paginate = (pageNumber) => setCurrentPage(pageNumber)
-  const endIdx = currentPage * itemsPerPage
-  const startIdx = endIdx - itemsPerPage
-
-  console.log('paginate data size', totalPages)
-  console.log('paginate start', startIdx)
-  console.log('paginate end', endIdx)
-  console.log('paginate current page', currentPage)
+  const itemsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(data?.length / itemsPerPage);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const endIdx = currentPage * itemsPerPage;
+  const startIdx = endIdx - itemsPerPage;
 
   useEffect(() => {
-    setData([...manga])
-  }, [manga])
+    setData([...manga]);
+  }, [manga]);
 
   // const { data: genresData } = useFetch(`/genre/${mediaType}/list`)
 
@@ -98,163 +93,163 @@ const Explore = ({ manga }) => {
   // }
 
   useEffect(() => {
-    filters = {}
+    filters = {};
     // setData(null)
-    setPageNum(1)
-    setSortby(null)
+    setPageNum(1);
+    setSortby(null);
     // setGenre(null)
     // fetchInitialData()
-  }, [mediaType])
+  }, [mediaType]);
 
   const onChange = (selectedItems, action) => {
     if (action.name === 'sortby') {
-      setSortby(selectedItems)
+      setSortby(selectedItems);
       if (action.action !== 'clear') {
-        filters.sort_by = selectedItems.value
+        filters.sort_by = selectedItems.value;
       } else {
-        delete filters.sort_by
+        delete filters.sort_by;
       }
     }
 
     if (action.name === 'genres') {
-      setGenre(selectedItems)
-      const genresLabel = genre.map((x) => x.label)
-      console.log('genres label', genresLabel)
-      const temp = data.filter((item) => item.genres.includes(genresLabel))
-      console.log('genres temp', temp)
+      setGenre(selectedItems);
+      const genresLabel = genre.map((x) => x.label);
+      console.log('genres label', genresLabel);
+      const temp = data.filter((item) => item.genres.includes(genresLabel));
+      console.log('genres temp', temp);
 
-      console.log('genres temp', temp)
+      console.log('genres temp', temp);
       if (action.action !== 'clear') {
         // let genreId = selectedItems.map((g) => g.id)
         // genreId = JSON.stringify(genreId).slice(1, -1)
         // filters.with_genres = genreId
       } else {
-        delete filters.with_genres
+        delete filters.with_genres;
       }
     }
 
-    setPageNum(1)
+    setPageNum(1);
     // fetchInitialData()
-  }
+  };
 
   const handleShowFilters = (btnName) => {
-    console.log('handleShowFilters', btnName)
+    console.log('handleShowFilters', btnName);
     if (btnName === 'genres') {
-      console.log('genres show')
-      setSelectedStatus(null)
-      setShowFilters((prev) => ({ genres: !prev.genres }))
+      console.log('genres show');
+      setSelectedStatus(null);
+      setShowFilters((prev) => ({ genres: !prev.genres }));
     } else if (btnName === 'status') {
-      console.log('status show')
-      setSelectedGenres([])
-      setShowFilters((prev) => ({ status: !prev.status }))
+      console.log('status show');
+      setSelectedGenres([]);
+      setShowFilters((prev) => ({ status: !prev.status }));
     } else if (btnName === 'added') {
-      console.log('handleShowFilter added show')
-      setShowFilters((prev) => ({ added: !prev.added }))
+      console.log('handleShowFilter added show');
+      setShowFilters((prev) => ({ added: !prev.added }));
     }
-  }
+  };
 
   // console.log('handleShowFilters', showFilters)
 
   const sortGenres = (e) => {
     // const selectedGenres = e.map((x) => x.label)
     // setData([...manga])
-    console.log('selectedGenres sortgenres called', selectedGenres)
-    console.log('selectedGenres manga', manga)
+    console.log('selectedGenres sortgenres called', selectedGenres);
+    console.log('selectedGenres manga', manga);
 
     const filteredData = manga.filter((item) =>
-      item.genres.some((genre) => selectedGenres.includes(genre))
-    )
+      item.genres.some((genre) => selectedGenres.includes(genre)),
+    );
 
-    console.log('filteredData genres', filteredData)
-    setData(filteredData)
+    console.log('filteredData genres', filteredData);
+    setData(filteredData);
 
     if (selectedGenres.length == 0) {
-      setData([...manga])
+      setData([...manga]);
     }
-  }
+  };
 
   const filterStatus = (e) => {
     // setData([...manga])
-    console.log('filterStatus status', selectedStatus)
-    console.log('filterStatus manga', manga)
-    const filteredData = manga.filter((item) => item.status === selectedStatus)
-    console.log('filteredData status', filteredData)
-    setData(filteredData)
+    console.log('filterStatus status', selectedStatus);
+    console.log('filterStatus manga', manga);
+    const filteredData = manga.filter((item) => item.status === selectedStatus);
+    console.log('filteredData status', filteredData);
+    setData(filteredData);
 
     if (!selectedStatus) {
-      setData([...manga])
+      setData([...manga]);
     }
-  }
+  };
 
   const filterAdded = (e) => {
-    console.log('filterAdded added', selectedAdded)
-    console.log('filterAdded manga', manga)
-    let filteredData = []
+    console.log('filterAdded added', selectedAdded);
+    console.log('filterAdded manga', manga);
+    let filteredData = [];
     if (selectedAdded === 'az') {
       //a-z sorting
       filteredData = manga.sort((a, b) => {
-        const nameA = a.title.toUpperCase()
-        const nameB = b.title.toUpperCase()
+        const nameA = a.title.toUpperCase();
+        const nameB = b.title.toUpperCase();
 
         if (nameA < nameB) {
-          return -1
+          return -1;
         }
 
         if (nameA > nameB) {
-          return 1
+          return 1;
         }
 
         // Names are equal
-        return 0
-      })
+        return 0;
+      });
     } else if (selectedAdded === 'za') {
       //a-z sorting
       filteredData = manga.sort((a, b) => {
-        const nameA = a.title.toUpperCase()
-        const nameB = b.title.toUpperCase()
+        const nameA = a.title.toUpperCase();
+        const nameB = b.title.toUpperCase();
 
         if (nameA > nameB) {
-          return -1
+          return -1;
         }
 
         if (nameA < nameB) {
-          return 1
+          return 1;
         }
 
         // Names are equal
-        return 0
-      })
+        return 0;
+      });
     }
-    console.log('filteredData added', filteredData)
-    setData(filteredData)
+    console.log('filteredData added', filteredData);
+    setData(filteredData);
 
     if (!selectedAdded) {
-      setData([...manga])
+      setData([...manga]);
     }
-  }
+  };
 
   return (
-    <div className='explorePage '>
+    <div className="explorePage ">
       <ContentWrapper>
-        <div className='relative'>
+        <div className="relative">
           <div
-            className='filters'
+            className="filters"
             style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.1)' }}
           >
             <button
-              className='p-1 bg-white filter'
+              className="p-1 bg-white filter"
               onClick={() => handleShowFilters('genres')}
             >
               Show Genres <DownArrow width={20} height={20} />
             </button>
             <button
-              className='p-1 bg-white filter'
+              className="p-1 bg-white filter"
               onClick={() => handleShowFilters('status')}
             >
               Status <DownArrow width={20} height={20} />
             </button>
             <button
-              className='p-1 bg-white filter'
+              className="p-1 bg-white filter"
               onClick={() => handleShowFilters('added')}
             >
               Order By Added <DownArrow width={20} height={20} />
@@ -278,9 +273,9 @@ const Explore = ({ manga }) => {
             onChange={filterAdded}
             show={showFilters.added}
           />
-          <div className='pageHeader'>
-            <div className='pageTitle'>Explore Manga</div>
-            <div className='filters'>
+          <div className="pageHeader">
+            <div className="pageTitle">Explore Manga</div>
+            <div className="filters">
               {/* <Select
                 isMulti
                 name='genres'
@@ -311,11 +306,11 @@ const Explore = ({ manga }) => {
           {data.length === 0 && <Spinner initial={true} />}
           {!loading && (
             <>
-              <div className='content'>
+              <div className="content">
                 {data?.slice(startIdx, endIdx).map((item, index) => {
                   return (
                     <MovieCard key={index} data={item} mediaType={mediaType} />
-                  )
+                  );
                 })}
               </div>
             </>
@@ -331,7 +326,7 @@ const Explore = ({ manga }) => {
         />
       </ContentWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default Explore
+export default Explore;
